dec952ddbab86eb499e6b4048fd6b31717591fd2
Christopher Pollati <abattoir@optonline.net>
2024-12-27 14:28:58 -0500

    Updated resume

--- resume.php
+++ resume.php
@@ -12,15 +12,15 @@ class Skills {
 }
 
 class WorkExperience {
-	public $company;
-	public $location;
-	public $dates;
-	public $title;
-	public $description;
-	public $bullets;
-	public $projects = "";
+	public string $company;
+	public string $location;
+	public string $dates;
+	public string $title;
+	public string $description;
+	public array $bullets;
+	public array $projects;
 
-	function __construct($company, $location, $dates, $title, $description, array $bullets, $projects = "") {
+	function __construct($company, $location, $dates, $title, $description, array $bullets, array $projects = []) {
 		$this->company = $company;
 		$this->location = $location;
 		$this->dates = $dates;
@@ -31,7 +31,7 @@ class WorkExperience {
 	}
 }
 
-function doEntry($name,$type,$location,$dates,$descrip) {
+function doEntry($name,$type,$location,$dates,$projects,$descrip) {
 ?>
 			<div class="row">
 				<div class="col-sm-6 col-md-6"><b><?=$name?></b>, <?=$location?></div>
@@ -41,6 +41,11 @@ function doEntry($name,$type,$location,$dates,$descrip) {
 			<div class="row">
 				<div class="col-md-12"><b><?=$type?></b></div>
 			</div>
+<?php foreach ($projects as $project) { ?>
+		<div class="row">
+			<div class="col-md-12"><?=$project?></div>
+		</div>
+<?php } ?>
 <?php if($descrip!="") {?>
 			<div class="row">
 				<div class="col-md-12"><?=$descrip?></div>
@@ -58,21 +63,6 @@ function doLineItems($items){
 	}
 ?>
 			</ul>
-	</div>
-<?php
-}
-
-function doItems($items,$projects = "") {
-	if($projects!="") { echo "<i>" . $projects . "</i><br/>"; }
-?>
-	<div class="row">
-		<ul>
-<?php
-	foreach ($items as $item) {
-		echo "<li>" . $item . "</li>";
-	}
-?>
-		</ul>
 	</div>
 <?php
 }
@@ -95,7 +85,7 @@ head_content("Resume","");
 				<span class="pull-right">
 					<a href="http://www.abattoir-software.com">www.abattoir-software.com</a>
 				</span>
-				<h3>Christopher Pollati <span class="text-muted">(formerly Kossa)</span></h3>
+				<h4 class="no-transform">Christopher Pollati <span class="text-muted">(formerly Kossa)</span></h4>
 				<hr style="margin: 3px 0 8px 0; border-top: 3px solid black;"/>
 				Howell, NJ 07731-2869
 				<span class="pull-right">
@@ -112,95 +102,121 @@ head_content("Resume","");
 			</div>
 		</div>
 	</div>
-	<h3>Game and Application Development | Workflow Performance Improvement</h3>
-	<p>Programmer with a specialty in digital, as well as traditional art, skilled in developing games or products to increase efficiencies. Thrives on learning how things work, how to make better, and how to educate or inform others. Experienced with various types of programming languages and software for art, 3D modeling, video editing, and office software, as well as several operating systems. Enjoys new challenges to enhance skill set.</p>
-	<h3>Skills</h3>
+	<h4 class="no-transform">Game and Application Development | Workflow Performance Improvement</h4>
+	<p>Programmer specializing in digital and traditional art, skilled in developing games and applications to increase efficiency and enhance learning. Thrives on understanding how things work, optimizing processes, and educating others. Experienced with diverse programming languages, development environments, and software tools. Adept at solving challenges and delivering innovative solutions.</p>
+	<h4>Skills</h4>
 <?php
 $skillItems = array (
-	new Skills("Programming", array("C#","PHP","Javascript","Java","ActionScript 3","Flex","Python","C/C++")),
-	new Skills("Development Software", array("<i>Eclipse</i>","<i>Visual Studio Code</i>","<i>Visual Studio</i>","<i>Xcode</i>","<i>IntelliJ/Android Studio</i>","<i>GitHub/GitLab</i>","<i>Git</i>","<i>SVN</i>","<i>Trello</i>","<i>Slack</i>","<i>Jira</i>")),
-	new Skills("Other Software",array("includes Adobe <i>Creative Cloud</i>","Document Foundation's <i>LibreOffice</i>","Microsoft <i>Office</i>","Microsoft <i>Office</i>","Google <i>G Suite</i>", "<i>Ubuntu</i> Linux","some experience in 3D modeling, video editing"))
+	new Skills("Programming", array("PHP","Javascript","C#","Java","ActionScript 3","Python","C/C++")),
+	new Skills("Development Software", array("<i>Visual Studio Code</i>","<i>Visual Studio</i>","<i>Xcode</i>","<i>Eclipse</i>","<i>IntelliJ/Android Studio</i>","Git","<i>GitHub</i>","<i>GitLab</i>","<i>Trello</i>","<i>Slack</i>","<i>Jira</i>)")),
+	new Skills("Other Software",array("<i>Ubuntu</i> Linux","<i>Figma</i>","Adobe <i>Creative Cloud</i>","<i>Google Workspace</i>","Microsoft <i>Office</i>","Document Foundation's <i>LibreOffice</i>"))
 );
 
 foreach ($skillItems as $skill) {
 	doRunningList($skill->title,$skill->skills);
 }
 ?>
-	<h3>Work Experience</h3>
+	<h4>Work Experience</h4>
 <?php
 $workExperienceItems = array(
-	new WorkExperience("KILLER SNAILS","New York, NY","2017 – Present","Lead Developer","Development of educational games and tools for evaluating their efficacy.",
+	new WorkExperience("KILLER SNAILS","Brooklyn, NY","Sep 2017 – Present",
+		"Director of Software Development",
+		"Develop educational games and tools for evaluating their efficacy.",
 		array(
-			"Produced several VR experiences using Unity that target Google Cardboard on iOS and Android",
-		    "Development of web portals for games to help teachers monitor student learning in real time",
-		    "Integrations of third-party APIs with account system for classroom management of games"
+			"Produced XR experiences using Unity that target Google Cardboard on iOS and Android.",
+		    "Developed systems connecting XR & websites to allow real-time monitoring of student learning.",
+		    "Integrated third-party APIs for classroom management systems."
+		),
+		array(
+			"BioDive (Android/iOS/WebGL), WaterWays (Android/iOS/WebGL)",
+			"U.S. Patent 11763691, <i>&quot;Method and learning system platform for extended reality digital hybrid education&quot;</i>, Sep 2023"
 		)
 	),
 
-	new WorkExperience("BLOOMFIELD COLLEGE","Bloomfield, NJ","2009 – Present","Adjunct Professor","Teach various classes for game programming ranging from Unity, XNA/C# and to ActionScript 3 for Flex, Flash and AIR to adapt to current languages and tools.",
+	new WorkExperience("ABATTOIR SOFTWARE LLC","Howell, NJ","Jul 2008 – Present",
+		"Owner / Programmer",
+		"Remote contract programming and consulting services.",
 		array(
-			"Teach different levels of Unity development, from basics to AI.",
-			"Led different levels of a class where student groups created games to facilitate teamwork.",
-			"Instructed class designed to help students learn about software and practices used in the games industry, preparing students for working in a studio environment."
+			"Modified Flash UIs, allowing communication with other SWFs.",
+			"Developed PHP scripts connecting Flex interfaces to MySQL databases.",
+			"Created cross-platform applications using Flex for web, desktop, and mobile"
+		),
+		array( "BlankWords (Android)" )
+	),
+
+	new WorkExperience("BLOOMFIELD COLLEGE","Bloomfield, NJ","Jan 2009 – May 2022",
+		"Adjunct Professor",
+		"Taught game programming courses including Unity, XNA/C# and to ActionScript 3 for Flex, Flash and AIR to adapt to current languages and tools.",
+		array(
+			"Led Unity classes from beginner to advanced levels, covering AI and gameplay mechanics.",
+			"Guided students in group projects simulating real-world game development workflows.",
+			"Provided insight into industry-standard software and practices."
 		)
 	),
 
-	new WorkExperience("ABATTOIR SOFTWARE LLC","Clifton, NJ","2008 – Present","Owner / Programmer","Executed contract programming, working remotely to assist other companies on game projects.",
+	new WorkExperience("PROPELLER COMMUNICATIONS","Summit, NJ","Jun 2016 – Sep 2017",
+		"Senior Front End Developer",
+		"Oversaw front end of web site development and iOS applications for various clients.",
 		array(
-			"Performed modification of Flash UIs, allowing communication with other SWFs.",
-			"Developed PHP scripts, bridging communications between Flex interfaces and MySQL databases for client websites.",
-			"Developed software using Flex, enabling common code base for web, desktop and mobile."
-		),"BlankWords (Android)"
-	),
-
-	new WorkExperience("PROPELLER COMMUNICATIONS","Summit, NJ","2016 – 2017","Senior Front End Developer","Oversaw front end of web site development and iOS applications for various clients.",
-		array(
-			"Prototyped a VR walkthroughs using Unity for Samsung GearVR and Google VR.",
-			"Maintenance of various programming projects across varied technologies.",
-			"Modified and expanded a web baser game built with Phaser.io"
+			"Prototyped VR walkthroughs using Unity for Samsung GearVR and Google Cardboard.",
+			"Maintained programming projects across varied technologies.",
+			"Enhanced web-based games with Phaser.io."
 		)
 	),
 
-	new WorkExperience("PEARSON","Hoboken, NJ","2010 – 2016","Digital Developer","Developed and maintained web applications for educational purposes, as well as development of tools to help produce content for those products.",
+	new WorkExperience("PEARSON","Hoboken, NJ","Sep 2010 – Feb 2016",
+		"Digital Developer",
+		"Designed and maintained web applications and content production tools.",
 		array(
-			"Developed desktop application utilizing MySQL database, streamlining management of tens-of-thousands of multimedia assets.",
-			"Created tool, simplifying batch re-encoding of videos and the embedding captions.",
-			"Designed web forms and PHP scripts, generating various static web pages from spreadsheets.",
-			"Used VMware vCloud Director to configure and set up web and database servers."
+			"Streamlined multimedia asset management with MySQL-integrated desktop apps.",
+			"Simplified video re-encoding and caption embedding with custom tools.",
+			"Automated static web page generation using spreadsheets and PHP scripts."
 		)
 	),
 
-	new WorkExperience("KENYON HOAG ASSOCIATES","Upper Saddle River, NJ","2009 – 2010","Web Developer / Programmer","",
+	new WorkExperience("KENYON HOAG ASSOCIATES","Upper Saddle River, NJ","Oct 2009 – Aug 2010",
+		"Web Developer / Programmer",
+		"Front end web development and animation focused on advertising for the pharma industry.",
 		array(
-			"Programmed Flash game, demonstrating company’s knowledge of the pharma industry.",
-			"Developed Flex tools for client websites, enabling product ordered to be tied to inventory system while streamlining ordering process.",
-			"Designed new or modified existing clients website based around corporate identities, increasing brand awareness."
-		),"Bioreactor Defender (Web)"
+			"Developed Flash game demonstrating company’s knowledge of the industry.",
+			"Created tools for client websites tying their inventory and product catalogue.",
+			"Designed client websites based around corporate identities."
+		),
+		array( "Bioreactor Defender (Web)" )
 	),
 
-	new WorkExperience("THQ / KAOS STUDIOS","New York City, NY","2006 – 2008","Programmer","Developed and maintained continuous builder for daily compiles of the PC / Xbox 360 executables.",
+	new WorkExperience("THQ / KAOS STUDIOS","New York City, NY","Oct 2006 – Apr 2008",
+		"Programmer",
+		"Developed tool to help developers the PC / Xbox 360 executables and game modifications.",
 		array(
-			"Wrote Python scripts and C# tools, allowing installation and build processes to be more efficient.",
-			"Modified Unreal 3 Engine, enabling Kismet to be used to control Bink video play back.",
-			"Merged updates from newer Unreal builds into older code base to add features to game."
-		),"Frontlines: Fuel of War (Xbox360, PC)"
+			"Wrote tools allowing installation and build processes to be more efficient.",
+			"Modified Unreal 3 Engine’s Kismet to be used to control Bink video playback.",
+			"Merged updates from newer Unreal builds into older code base."
+		),
+		array( "Frontlines: Fuel of War (Xbox360, PC)" )
 	)
 );
 
 foreach ($workExperienceItems as $item) {
-	doEntry($item->company, $item->title, $item->location, $item->dates, $item->description);
-	doItems($item->bullets, $item->projects);
+	doEntry($item->company, $item->title, $item->location, $item->dates, $item->projects, $item->description);
+	doLineItems($item->bullets);
 }
 
-$achievments = array(
+$recognitions = array(
 	"Member of the International Game Developers Association (IGDA) since 2006",
 	"Presenter, New Jersey Chapter of the IGDA",
 	"IGDA Game Developers Conference 2007 Scholarship recipient",
+);
+$achievments = array(
 	"<b>Bachelor of Arts (BA)</b>, Game Development – Programming, Bloomfield College, Bloomfield, NJ, <i>summa cum laude</i>, GPA: 3.894<ul><li>Dean’s List</li><li>Developed a playable demo of a 3D, multiplayer game with Torque Game Engine in three months for Capstone Project</li></ul>",
 	"<b>Associate of Applied Science (AAS)</b>, Commercial Graphics / Computer Animation, Bergen Community College, Paramus, NJ<ul><li>Curricular Award of Excellence in Computer Animation</li></ul>"
 );
 ?>
-	<h3>Education and Achievements</h3>
+	<h4>Professional Memberships and Recognitions</h4>
+<?php
+	doLineItems($recognitions);
+?>
+	<h4>Education and Achievements</h4>
 <?php
 	doLineItems($achievments);
 ?>
@@ -209,14 +225,20 @@ $achievments = array(
 	<div>
 		<p class="text-muted">Page last update: <?php echo date("F d, Y"/* H:i"*/, getlastmod()); ?></p>
 	</div>
+	<hr/>
 	<div>
 		<a class="pull-right" href="https://get.adobe.com/reader/"><img src="assets/images/layout/Get_Adobe_Acrobat_Reader_DC_web_button_158x39.fw.png"></a>
+		<h4>One Page</h4>
+		<a href="assets/downloads/resume/Resume-ChristopherPollati-OnePage.pdf" target="_blank"><img src="assets/images/layout/icons/pdf.png"/> Christopher Pollati's One Page Resume (PDF)</a>
+		<p class="text-muted">Last updated: <?php echo date("F d, Y"/* H:i"*/, filemtime("assets/downloads/resume/Resume-ChristopherPollati-OnePage.pdf")); ?></p>
+		<a href="assets/downloads/resume/Resume-ChristopherPollati-OnePage.docx" target="_blank"><img src="assets/images/layout/icons/word.png"/> Christopher Pollati's One Page Resume (Word Docx)</a>
+		<p class="text-muted">Last updated: <?php echo date("F d, Y"/* H:i"*/, filemtime("assets/downloads/resume/Resume-ChristopherPollati-OnePage.docx")); ?></p>
+		<h4>Full</h4>
 		<a href="assets/downloads/resume/Resume-ChristopherPollati.pdf" target="_blank"><img src="assets/images/layout/icons/pdf.png"/> Christopher Pollati's Resume (PDF)</a>
 		<p class="text-muted">Last updated: <?php echo date("F d, Y"/* H:i"*/, filemtime("assets/downloads/resume/Resume-ChristopherPollati.pdf")); ?></p>
 		<a href="assets/downloads/resume/Resume-ChristopherPollati.docx" target="_blank"><img src="assets/images/layout/icons/word.png"/> Christopher Pollati's Resume (Word Docx)</a>
 		<p class="text-muted">Last updated: <?php echo date("F d, Y"/* H:i"*/, filemtime("assets/downloads/resume/Resume-ChristopherPollati.docx")); ?></p>
 	</div>
-	<div>
-	</div>
+	<div></div>
 </div>
 <?php end_content(); ?>
